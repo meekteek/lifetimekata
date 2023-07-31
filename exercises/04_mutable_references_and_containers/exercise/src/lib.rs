58fe7c4d1acd4f0be880604fdba38557745c2088
meekteek <filledtotofucrumbs@gmail.com>
2023-07-31 12:58:27 -0700

    complete ex 4/5

--- exercises/04_mutable_references_and_containers/exercise/src/lib.rs
+++ exercises/04_mutable_references_and_containers/exercise/src/lib.rs
@@ -31,8 +31,7 @@ use require_lifetimes::require_lifetimes;
 /// assert_eq!(message , vec!["Hello", "Your", "Name", "Is", "Unknown"]);
 /// ````
 #[require_lifetimes(!)]
-pub fn vector_set(vector: &mut Vec<&str>, loc: usize, new: &str) {
-    // TODO: You will need to write this code yourself.
-    //       Don't worry, it's only one line long.
+pub fn vector_set<'a, 'b>(vector: &'a mut Vec<&'b str>, loc: usize, new: &'b str) {
+    vector[loc] = new;
     todo!()
 }
